477ffa2db2cb832ced96ae511b552e502556e69a
Softwarica College Lab <softwarica@softwarica.com>
2019-05-21 11:43:29 +0545

    Final add

--- app/src/main/java/com/example/heroesapi/MainActivity.java
+++ app/src/main/java/com/example/heroesapi/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.heroesapi;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 import Api.Heroes;
 import URL.url;
@@ -19,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText etName, etDesc;
     private Button btnSave;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,57 +34,78 @@ public class MainActivity extends AppCompatActivity {
 
         etName = findViewById(R.id.etName);
         etDesc = findViewById(R.id.etDesc);
-        btnSave=findViewById(R.id.btnSave);
+        btnSave = findViewById(R.id.btnSave);
+        img = findViewById(R.id.img);
+   loadFormURL();
 
-      btnSave.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              Save();
-          }
-
-
-      });
-
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Save();
+            }
 
 
+        });
+
+
+    }
+
+    private void StrictMode(){
+        android.os.StrictMode.ThreadPolicy threadPolicy = new android.os.StrictMode.ThreadPolicy.Builder().permitAll().build();
+        android.os.StrictMode.setThreadPolicy(threadPolicy);
+
+    }
+
+    private void loadFormURL() {
+
+        StrictMode();
+        try {
+            String imgURL ="";
+            URL url = new URL(imgURL);
+            img.setImageBitmap(BitmapFactory.decodeStream(InputStream)url.getContent());
+
+
+        }
+        catch (IOException e){
+            Toast.makeText(this,"Error",Toast.LENGTH_LONG).show();
+        }
     }
 
 
     private void Save() {
 
 
-            String name = etName.getText().toString();
-            String desc = etDesc.getText().toString();
+        String name = etName.getText().toString();
+        String desc = etDesc.getText().toString();
 
 
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(url.BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-            Heroes api = retrofit.create(Heroes.class);
+        Heroes api = retrofit.create(Heroes.class);
 
-            Call<Void> heroesCall = api.addHero(name,desc);
+        Call<Void> heroesCall = api.addHero(name, desc);
 
-            heroesCall.enqueue(new Callback<Void>() {
-                @Override
-                public void onResponse(Call<Void> call, Response<Void> response) {
+        heroesCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
 
-                    if(!response.isSuccessful())
-                    {
-                        Toast.makeText(MainActivity.this, "Code", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                    Toast.makeText(MainActivity.this, "Successfully added", Toast.LENGTH_SHORT).show();
+                if (!response.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Code", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+                Toast.makeText(MainActivity.this, "Successfully added", Toast.LENGTH_SHORT).show();
+            }
 
-                @Override
-                public void onFailure(Call<Void> call, Throwable t) {
-                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+}
 
 
 
