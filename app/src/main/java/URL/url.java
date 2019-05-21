a831c93cc7c44b9afcf30f2a5198bfd53951aed5
Softwarica College Lab <softwarica@softwarica.com>
2019-05-21 11:29:23 +0545

    Final add

--- app/src/main/java/URL/url.java
+++ app/src/main/java/URL/url.java
@@ -2,5 +2,5 @@ package URL;
 
 public class url {
 
-    public static final String BASE_URL = "10.0.2.2:3000/";
+    public static final String BASE_URL = "http://10.0.2.2:3000/";
 }
--- app/src/main/java/com/example/heroesapi/MainActivity.java
+++ app/src/main/java/com/example/heroesapi/MainActivity.java
@@ -42,41 +42,42 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void Save(){
+
+    private void Save() {
 
 
-        String name = etName.getText().toString();
-        String desc = etDesc.getText().toString();
+            String name = etName.getText().toString();
+            String desc = etDesc.getText().toString();
 
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(url.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(url.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
 
-        Heroes api = retrofit.create(Heroes.class);
+            Heroes api = retrofit.create(Heroes.class);
 
-        Call<Void> heroesCall = api.addHero(name,desc);
+            Call<Void> heroesCall = api.addHero(name,desc);
 
-        heroesCall.enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
+            heroesCall.enqueue(new Callback<Void>() {
+                @Override
+                public void onResponse(Call<Void> call, Response<Void> response) {
 
-                if(!response.isSuccessful())
-                {
-                    Toast.makeText(MainActivity.this, "Code", Toast.LENGTH_SHORT).show();
-                    return;
+                    if(!response.isSuccessful())
+                    {
+                        Toast.makeText(MainActivity.this, "Code", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Toast.makeText(MainActivity.this, "Successfully added", Toast.LENGTH_SHORT).show();
                 }
-                Toast.makeText(MainActivity.this, "Successfully added", Toast.LENGTH_SHORT).show();
-            }
 
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-            }
-        });
+                @Override
+                public void onFailure(Call<Void> call, Throwable t) {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
 
 
-}
